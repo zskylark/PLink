a351daea0ad01edbdec0e3b769affd96bbd67dba
zskylark <zskylark@sina.com>
2013-07-31 15:18:22 +0800

    update win32/android library.

--- 1.0.2/libs/android/Channel.java
+++ 1.0.2/libs/android/Channel.java
@@ -50,11 +50,22 @@ public class Channel {
 	//////////////////////////////////////////////////////////////////////////
 	// node channel synchronous communication interface.
 	static public native int send(String fr, String to, byte[] snd_buf, int snd_len, byte[] rcv_buf, int[] rcv_len, int timeout_ms);
+		/// rcv_len: set rcv_buf max size to receiver, when it return, it be set by really receive length.
+	static public native int ack(String fr, String to, int params, byte[] buf, int len, int timeout_ms);
 	
 	
 	//////////////////////////////////////////////////////////////////////////
 	// node channel asynchronous communication interface.
 	static public native int post(String fr, String to, byte[] buf, int len, int timeout_ms);
+	static public native int get(int[] type, int[] params, byte[] fr, int[] fr_len, byte[] to, int[] to_len, byte[] buf, int[] buf_len, int timeout_ms);
+		/// fr_len, to_len, buf_len: set buffer max size to receiver, when function return, it be set by really buffer length.
+	    /// Type_Chl_Post = 1,
+	    /// Type_Chl_Send = 2,
+	    /// Type_Chl_Online = 3,
+	    /// Type_Chl_Offline = 4,
+	    /// Type_Chl_Join = 5,
+	    /// Type_Chl_ReJoin = 6,
+	    /// Type_Chl_Abort = 7
     
     
 	//////////////////////////////////////////////////////////////////////////
